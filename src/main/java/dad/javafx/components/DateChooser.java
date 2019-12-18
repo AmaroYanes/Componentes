20f41fe0c4947e84d6f42d53e56c989125ea0a39
AmaroYanes <ihvamaroyanes@gmail.com>
2019-12-18 10:52:57 +0000

    Actualizacion codigo

--- src/main/java/dad/javafx/components/DateChooser.java
+++ src/main/java/dad/javafx/components/DateChooser.java
@@ -2,6 +2,7 @@ package dad.javafx.components;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.time.Year;
 import java.util.Calendar;
 import java.util.ResourceBundle;
@@ -16,7 +17,6 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -29,6 +29,9 @@ public class DateChooser extends HBox  implements Initializable{
 	private ObjectProperty<Meses> monthProperty = new SimpleObjectProperty<Meses>();
 	private IntegerProperty dayProperty = new SimpleIntegerProperty();
 	private IntegerProperty yearProperty = new SimpleIntegerProperty();
+	private StringProperty conversorYear = new SimpleStringProperty();
+	private ObjectProperty<LocalDate> dateProperty = new SimpleObjectProperty<LocalDate>();
+	
 	
 	//view
     @FXML
@@ -73,27 +76,40 @@ public class DateChooser extends HBox  implements Initializable{
 		}
 		
 		monthsList.addAll(Meses.values());
+		dateProperty.addListener((o,ov,nv) ->{
+			dayProperty.set(nv.getDayOfMonth());	
+			monthProperty.set(Meses.valueOf(nv.getMonth().toString()));
+			yearProperty.set(nv.getYear());
+			
+		});
+		monthProperty.addListener((o,ov,nv) -> {
+			monthsCombo.getSelectionModel().select(nv);
+			daysList.clear();
+			int i;
+			for(i = 0; i <= Meses.numDias(nv);i++) {
+				daysList.add(i);
+			}if(nv.equals(Meses.Febrero) && Year.of(yearProperty.get()).isLeap())
+					daysList.add(i+1);
+			
+		});
+		yearProperty.addListener((o,ov,nv) -> {
+			yearsCombo.getSelectionModel().select(nv.intValue());
+			daysList.clear();
+			int i;
+			for(i = 0; i <= Meses.numDias(monthProperty.get());i++) {
+				daysList.add(i);
+			}if(monthProperty.get().equals(Meses.Febrero) && Year.of(nv.intValue()).isLeap())
+					daysList.add(i+1);
+		});
+		dayProperty.addListener((o,ov,nv) -> {
+			daysCombo.getSelectionModel().select(nv.intValue());
+		});
+		iniciar();
 		
-//		monthProperty.addListener((o,ov,nv) -> {
-//			daysList.clear();
-//			int i;
-//			for(i = 0; i <= Meses.numDias(nv);i++) {
-//				daysList.add(i);
-//			}if(nv.equals(Meses.Febrero) && Year.of(yearProperty.get()).isLeap())
-//					daysList.add(i+1);
-//			
-//		});
-//		yearProperty.addListener((o,ov,nv) -> {
-//			daysList.clear();
-//			int i;
-//			for(i = 0; i <= Meses.numDias(monthProperty.get());i++) {
-//				daysList.add(i);
-//			}if(monthProperty.get().equals(Meses.Febrero) && Year.of(nv.intValue()).isLeap())
-//					daysList.add(i+1);
-//		});
+	}
+	public void iniciar() {
 		monthsCombo.getSelectionModel().select(0);
 		yearsCombo.getSelectionModel().select(0);
-		
 	}
 	private enum Meses{
 		Enero,Febrero,Marzo,Abril,Mayo,Junio,Julio,Agosto,
@@ -109,5 +125,20 @@ public class DateChooser extends HBox  implements Initializable{
 			}
 		}
 	}
+	public final ObjectProperty<LocalDate> datePropertyProperty() {
+		return this.dateProperty;
+	}
+	
+	public final LocalDate getDateProperty() {
+		return LocalDate.of(yearProperty.get(), monthProperty.get().ordinal()+1, dayProperty.get());
+	}
+	
+	public final void setDateProperty(final LocalDate dateProperty) {
+		Meses[] meses = Meses.values();
+		monthsCombo.getSelectionModel().select(meses[dateProperty.getMonthValue()-1]);
+		yearsCombo.getSelectionModel().select( new Integer(dateProperty.getYear()));
+		daysCombo.getSelectionModel().select(dateProperty.getDayOfMonth());
+	}
+	
 
 }
--- src/main/java/dad/javafx/components/TestApp.java
+++ src/main/java/dad/javafx/components/TestApp.java
@@ -1,25 +1,59 @@
 package dad.javafx.components;
 
+import java.time.LocalDate;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TestApp extends Application {
 
-	@Override
+	DateChooser date = new DateChooser();
 	public void start(Stage primaryStage) throws Exception {
 
 		
+		
+		Button checkButton = new Button("Inicializar");
+		checkButton.setOnAction(e -> onIniciar(e));
+		
+		Button consultaButton = new Button("Consultar");
+		consultaButton.setOnAction(e -> onConsultar(e));
+		
+		HBox botones = new HBox();
+		botones.setSpacing(5);
+		botones.setAlignment(Pos.CENTER);
+		botones.getChildren().addAll(checkButton,consultaButton);
+		
+		VBox todo = new VBox();
+		todo.setSpacing(5);
+		todo.setAlignment(Pos.CENTER);
+		todo.getChildren().addAll(date,botones);
+		
 		BorderPane root = new BorderPane();
-		root.setCenter(new DateChooser());
+		root.setCenter(todo);
+
+
 		
-		Scene scene = new Scene(root,600,400);
+		Scene scene = new Scene(root,300,200);
 		
-		primaryStage.setTitle("Custom com");
+		primaryStage.setTitle("Probando JDate");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
+	}
+
+	private void onConsultar(ActionEvent e) {
+		System.out.println(date.getDateProperty());
+	}
+
+	private void onIniciar(ActionEvent e) {
+		date.setDateProperty(LocalDate.now());
 	}
 	public static void main(String[] args) {
 		launch(args);
